7467b4c46d561872f9912718f0f40f41a5d8f6b5
kelvyn-1 <99338834+kelvyn-1@users.noreply.github.com>
2022-03-09 10:32:40 -0300

    Create codigo.ino

---
+++ aula1/codigo.ino
@@ -0,0 +1,32 @@
+// código C++
+//
+ configuração de vazio ()
+{
+  pinMode ( 2 ,INPUT);
+    pinMode ( 3 ,SAÍDA);
+    Serial. começar ( 9600 );
+}
+
+ loop vazio ()
+{
+    bool botao = digitalRead ( 2 );
+    digitalWrite ( 3 ,botão);
+    atraso ( 10 );
+    Serial. println (botao);
+}
+// código C++
+//
+ configuração de vazio ()
+{
+  pinMode ( 2 ,INPUT);
+    pinMode ( 3 ,SAÍDA);
+    Serial. começar ( 9600 );
+}
+
+ loop vazio ()
+{
+    bool botao = digitalRead ( 2 );
+    digitalWrite ( 3 ,botão);
+    atraso ( 10 );
+    Serial. println (botao);
+}
